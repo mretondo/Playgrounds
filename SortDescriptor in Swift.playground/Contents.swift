910f6a1e1394b6bbc92b066c6c86ecb63adc6694
Mike Retondo <mretondo@mac.com>
2020-11-25 20:34:24 -0600

    Function rename and comment changes

--- SortDescriptor in Swift.playground/Contents.swift
+++ SortDescriptor in Swift.playground/Contents.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 //
-// Swift version of Foundations NSSortDescriptor
+// Swift version of Foundations NSSortDescriptor but Type safe
 //
 
-/// A sorting predicate that returns `true` if the first
-/// value should be ordered before the second.
+// A sorting predicate that returns `true` if the first
+// value should be ordered before the second.
 typealias SortDescriptor<Root> = (Root, Root) -> Bool
 
 // Function that return a Boolean value, because that's the standard library's convention for comparison predicates
@@ -36,11 +36,12 @@ func sortDescriptor<Root, Value>(
     }
 }
 
-// Function that combines multiple sort descriptors into a single sort descriptor.
-//
-// First it tries the first descriptor and uses that comparison result. However, if the result is equal,
-// it uses the second descriptor, and so on, until we run out of descriptors.
-func combineSortDescriptors<Root> (_ sortDescriptors: [SortDescriptor<Root>]) -> SortDescriptor<Root>
+/// Combines multiple sort descriptors into a single sort descriptor.
+/// First it tries the first descriptor and uses that comparison result.
+/// However, if the result is equal, it uses the second descriptor, and
+/// so on, until we run out of descriptors.
+/// - Parameter sortDescriptors: [SortDescriptor]
+func combine<Root> (sortDescriptors: [SortDescriptor<Root>]) -> SortDescriptor<Root>
 {
     return { lhs, rhs in
         for areInIncreasingOrder in sortDescriptors {
@@ -48,6 +49,7 @@ func combineSortDescriptors<Root> (_ sortDescriptors: [SortDescriptor<Root>]) ->
                 return true
             }
 
+            // flip lhs and rhs order
             if areInIncreasingOrder(rhs, lhs) {
                 return false
             }
@@ -57,65 +59,40 @@ func combineSortDescriptors<Root> (_ sortDescriptors: [SortDescriptor<Root>]) ->
     }
 }
 
-// Take a regular comparison function such as localizedStandardCompare, which works
-// on two strings, and turn it into a function that takes two optional strings
+///
+/// lift() allows us to “lift” a regular comparison function into the domain of optionals, and
+/// it can be used together with our sortDescriptor function. It takes a regular comparison
+/// function such as String:localizedStandardCompare(_:), which works on two objects, 'self'
+/// and the object passed to it. It then turns it into a function that takes two optional
+/// objects e.g. (lhs: String?, rhs: String?) -> ComparisonResult.
+///
+/// Example:
+/// extension String {
+///     var fileExtension: String? {
+///         guard let period = lastIndex(of: ".") else { return nil }
+///
+///         let extensionStart = index(after: period)
+///         return String(self[extensionStart...])
+///     }
+/// }
+///
+///  var files = ["file.swift", "one", "two", "test.h", "three", "file.h", "file.", "file.c"]
+///  let compare = lift(String.localizedStandardCompare)
+///      'compare(lhs: String?, rhs: String?) -> ComparisonResult'
+///  let result = files.sorted(by: sortDescriptor(key: { $0.fileExtension }, by: compare))
+///      result equals ["one", "two", "three", "file.", "file.c", "test.h", "file.h", "file.swift"]
+///
+/// - Parameter compare: a regular comparison compare function such as String:localizedStandardCompare(_:)
+/// - Returns: A ComparisonResult.
 func lift<A>(_ compare: @escaping (A) -> (A) -> ComparisonResult) -> (A?) -> (A?) -> ComparisonResult
 {
     return { lhs in { rhs in
         switch (lhs, rhs) {
-        case (nil, nil): return .orderedSame
-        case (nil, _): return .orderedAscending
-        case (_, nil): return .orderedDescending
-        case let (l?, r?): return compare(l)(r)
+            case (nil, nil): return .orderedSame
+            case (nil, _): return .orderedAscending
+            case (_, nil): return .orderedDescending
+            case let (l?, r?): return compare(l)(r)
         }
-    } }
+    }}
 }
 
-
-@objcMembers
-final class Person: NSObject
-{
-    let first: String
-    let last: String
-    let yearOfBirth: Int
-
-    init(first: String, last: String, yearOfBirth: Int)
-    {
-        self.first = first
-        self.last = last
-        self.yearOfBirth = yearOfBirth
-        // super.init() implicitly called here
-    }
-
-    override var debugDescription: String {
-        return first + " " + last + " " + String(yearOfBirth)
-    }
-}
-
-extension Person
-{
-    override var description: String { "\(first) \(last) \(yearOfBirth)" }
-}
-
-//
-// Define an array of people with different names and birth years
-//
-let people = [
-    Person(first: "Emily",  last: "Young",  yearOfBirth: 2002),
-    Person(first: "David",  last: "Gray",   yearOfBirth: 1991),
-    Person(first: "Robert", last: "Barnes", yearOfBirth: 1985),
-    Person(first: "Ava",    last: "Barnes", yearOfBirth: 2000),
-    Person(first: "Joanne", last: "Miller", yearOfBirth: 1994),
-    Person(first: "Ava",    last: "Barnes", yearOfBirth: 1998),
-]
-
-let sortByYear: SortDescriptor<Person> = sortDescriptor(key: { $0.yearOfBirth })
-let sortByFirstName: SortDescriptor<Person> = sortDescriptor(key: { $0.first }, by: String.localizedStandardCompare)
-let sortByLastName: SortDescriptor<Person> = sortDescriptor(key: { $0.last }, by: String.localizedStandardCompare)
-var combinedSortDescriptors: SortDescriptor<Person> = combineSortDescriptors( [sortByLastName, sortByFirstName, sortByYear] )
-let orderedPeople = people.sorted(by: combinedSortDescriptors).description
-
-let sortByYearGreaterThan: SortDescriptor<Person> = sortDescriptor(key: { $0.yearOfBirth }, by: >)
-combinedSortDescriptors = combineSortDescriptors( [sortByLastName, sortByFirstName, sortByYearGreaterThan] )
-//print (people)
-people.sorted(by: combinedSortDescriptors).description
